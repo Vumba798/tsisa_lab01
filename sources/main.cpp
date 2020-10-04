c834d87abaa7002c24c52641131c32e47480921b
Vumba798 <alexandrov32649@gmail.com>
2020-10-04 16:20:32 +0300

    formatted print

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,9 +8,15 @@ using std::cout;
 using std::cin;
 
 int main(int argc, char** argv){
+    cout << "The function is: func(x) = -0.5*cos(0.5x) - 0.5" << endl << endl;
+    cout << endl << "\t\t\tPASSIVE SEARCHER" << endl;
+    cout << "===========================================================" << endl;
     PassiveSearcher ps(1, 4);
     ps.print();
 
+    cout << endl << endl << endl << "\t\t\t\t\tDICHOTOMY SEARCHER" << endl;
+    cout << "========================================================";
+    cout << "===================================================" << endl;
     DichotomySearcher ds(1, 4);
     ds.print();
     return 0;
